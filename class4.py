a2d23418032bf759fc41a190efb25f351881f5c2
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-09-20 10:25:08 +0530

    Update class4.py

--- class4.py
+++ class4.py
@@ -1,4 +1,17 @@
-#set
+#set---------- {} / set()
+
+set={2,3,6,5,4,7,2,7,5,5,1}
+print(set)
+
+thisset={'apple','banana','cherry'}
+thisset.discard(5)
+print(thisset)
+
+a,b= {2,45,7,9,23},{43,6,7,9,2}
+print(a.difference(b))
+print(a)
+b.difference_update(a)
+print(b)
 
 s={20,30,40,50,60,70,30,40,10}
 print(type(s))
